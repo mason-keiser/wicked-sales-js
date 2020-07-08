5609df2aa687aa4a2baaf60fa7d75a1634d9888f
Mason Keiser <masonksr5@gmail.com>
2020-07-08 21:31:54 +0000

    user can add to cart (front-end)

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 class Header extends React.Component {
     render() {
+  
         return(
             <div className='navbar'>
                 <div className='titleContainer ml-5'>
                     <div className='title'><span className='fas fa-dollar-sign mr-2'></span>Wicked Sales</div>
+                </div>
+                <div className="row text-white">
+                  <p>{this.props.cartNumber} item(s)</p>
+                  <i className="fas fa-shopping-cart fa-lg text-white ml-2" onClick={() => this.props.setView('cart', {})}></i>
                 </div>
             </div>
         )   
