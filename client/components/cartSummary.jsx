7881ccfaba0a7d06ed6d14488cb913ec894a6f97
Mason Keiser <masonksr5@gmail.com>
2020-07-14 01:01:56 +0000

    progress

--- client/components/cartSummary.jsx
+++ client/components/cartSummary.jsx
@@ -5,11 +5,12 @@ export default class CartSummary extends React.Component {
     render() {
         if (this.props.products.length === 0) {
           return (
-            <div>
-              <div onClick={() => this.props.setView('catalog', {})}>&lt; Back to Catalog</div>
-              <h1 className="cart-title">My Cart</h1>
-              <div>
-                <h1>Your Cart is currently empty</h1>
+            <div className='container modalContainer'>
+              <div className="modalColumn">
+                <h1 className='ml-5 pl-4'>Your cart is empty!</h1>
+                <img className="gif" src="https://media.giphy.com/media/3o6Zt4j96fDG4XzO0w/source.gif" height="300"width="480" frameBorder="0" href="https://giphy.com/gifs/94EQmVHkveNck"/>
+                <h5 className="mt-2">Add items to your cart by selecting the add to cart button on each catalog item!</h5>
+                <div className="mt-2" onClick={() => this.props.setView('catalog', {})}>&lt; back to catalog</div>
               </div>
             </div>
           );
