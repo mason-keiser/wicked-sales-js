ba43f91e4795e0472fab4f939570ea3f1e51c64a
Mason Keiser <masonksr5@gmail.com>
2020-09-09 03:32:00 +0000

    white

--- client/components/disclaimer.jsx
+++ client/components/disclaimer.jsx
@@ -25,9 +25,6 @@ export default class Disclaimer extends React.Component {
           width: '100%',
           opacity: '90%'
         }
-        const black = (window.screen.width < 600)
-        ? { color: 'white'}
-        : { color: 'white'}
         const backgroundImg = (window.screen.width < 600)
         ? <img src="images/Moble.jpg" alt="homePage" style={height} />
         :    <img src="images/mountain.jpg" alt="homePage" style={height} />
@@ -36,7 +33,7 @@ export default class Disclaimer extends React.Component {
             <div className=' col-xs-12'>
                  {backgroundImg}
               <div className="" id='disclaimer' >
-                <h1 className='mb-3' style={black}>This site was made for demonstration purposes only!</h1>
+                <h1 className='mb-3' style={{ color: 'white'}}>This site was made for demonstration purposes only!</h1>
                 <h5 className="mt-2" style={{ color: 'white'}}>Please do not enter any personal information as any orders placed are void since the site is just a demo. By clicking the button below you acknowledge these terms.</h5>
                 <button className="mt-2 addToCart" onClick={() => this.props.setView('catalog', {})}>I consent</button>
               </div>
