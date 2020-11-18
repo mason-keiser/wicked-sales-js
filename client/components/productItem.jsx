2211e5a738b59f2b530b416e1f743f1689fbf990
Mason Keiser <masonksr5@gmail.com>
2020-11-18 04:00:22 +0000

    product updates

--- client/components/productItem.jsx
+++ client/components/productItem.jsx
@@ -7,7 +7,7 @@ export default class ProductItem extends React.Component {
         return (
           <div className="col-md-6 col-lg-4 d-flex align-items-stretch mt-4 mb-2 fade-in" id={product.productId}>
             <div className="card" onClick={() => this.props.view('details', { productId: this.props.product.productId})}>
-              <img src={product.image} className="card-img-top h-50 cardImg" alt={product.name} />
+              <img src={product.image} className="card-img-top cardImg" alt={product.name} />
               <div className="card-body">
                 <h5 className="card-title">{product.name}</h5>
                 <p className="card-price font-weight-bold">{`$${(this.props.product.price / 100).toFixed(2)}`}</p>
